64ce42209ab41540ed16531036498c8b69e1403f
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2026-08-04 21:38:50 -0300

    Inicialização da service de livros.

--- src/main/java/br/com/felipe/springoteca/services/LivroService.java
+++ src/main/java/br/com/felipe/springoteca/services/LivroService.java
@@ -4,17 +4,32 @@ import br.com.felipe.springoteca.models.LivroModel;
 import br.com.felipe.springoteca.repositorys.LivroRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LivroService {
-    public final LivroRepository livroRepository;
+    private final LivroRepository livroRepository;
 
+    //construtor do repository do livro
     public LivroService(LivroRepository livroRepository) {
         this.livroRepository = livroRepository;
     }
 
+    //buscar um livro pelo id
     public LivroModel getById(Long id)
     {
         return livroRepository.findById(id).orElse(null);
     }
 
+    //cadastrar um livro
+    public LivroModel cadastrarLivro(LivroModel livro)
+    {
+        return livroRepository.save(livro);
+    }
+
+    //buscar todos os livros
+    public List<LivroModel> listarLivros()
+    {
+        return livroRepository.findAll();
+    }
 }
